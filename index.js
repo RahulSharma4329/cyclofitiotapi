dd59758950a97d9599741e7556fe394c3ac79f1c
RahulSharma4329 <rahulsharma4329@gmail.com>
2022-11-23 20:23:49 +0530

    4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const axios = require("axios");
-const port = process.env.PORT || 5000;
+const port = 5000;
 const cors = require("cors");
 
 const app = express();
